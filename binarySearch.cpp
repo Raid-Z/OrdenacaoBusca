0b8f0a829f6c93e90adf7fbca704ca07bf2e3619
André Cano <andrerenatofc@gmail.com>
2023-03-07 19:21:49 -0300

    Identação

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,8 +1,10 @@
-int binarySearch(int *arr, int len) {
+int binarySearch(int *arr, int len)
+{
 	bSearch(arr, 0, len);
 }
 
-int bSearch(int *arr, int start, int end)  {
+int bSearch(int *arr, int start, int end)
+{
 	// TODO: Implement me
 	return 0;
 }
