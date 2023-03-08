785adf2cabbc12f66f78ff625c713a97d8fe6059
LuisFelipeMarrote <luisfelipe.marrote@gmail.com>
2023-03-07 21:22:11 -0300

    Atualização do main para criar a array randomica

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+using namespace std;
 
-void randomArray(int len, int *arr)
+int* randomArray(int len)
 {
-    arr = new int[len];
-    srand(time(0));
-    for (int i = 0; i < len; i++)
-    {
-        arr[i] = rand() % 100; // generate a random integer between 0 and 99
+    int *arrC = new int[len];
+    for (int i=0; i <= len;i++){
+        arrC[i] = 0;
     }
+    srand(time(0));
+    for (int i = 0; i < len; i++){
+        arrC[i] = 20 + rand()%1999980; // generate a random integer between 20 and 2000000
+    }
+    return arrC;
 }
 
 int main()
 {
-    int *arr;
-    randomArray(1000000, arr);
+    int **arr;
+    arr = new int * [4];
+    int tamanhoVet = 0;
+    int numeroProcurado = 0;
+    int k;
+    for (k = 0; k <= 4; k++){
+        srand(time(0));
+        tamanhoVet = 10000 + rand()%990000;
+        arr[k] = new int [tamanhoVet];
+        arr[k] = randomArray(tamanhoVet);
+    }
+
+    do{
+        cout << "Procure um numero no array:" << endl;
+        cin >> numeroProcurado;
+    } while(!(numeroProcurado > 20 && numeroProcurado < 2000000));
+
+
+    for (int i=0; i <= 4;i++){
+        delete[] arr[i];
+    }
+
+    return 0;
 }
