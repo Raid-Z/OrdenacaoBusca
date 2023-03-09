677572efdb8bbaa24d3f3ab6c6bf1b3fa7199a24
André Cano <andrerenatofc@gmail.com>
2023-03-08 21:55:48 -0300

    Test functionality

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include "sort.hpp"
+#include "search.hpp"
+
+int* randomArray(int len);
 
 int main()
 {
-    int *arr;
-    randomArray(1000000, arr);
+    int *arr = randomArray(10);
+    quickSort(arr,10);
+    std::cout << ternarySearch(100, arr, 10);
+    return 0;
 }
 
-void randomArray(int len, int *arr)
+int* randomArray(int len)
 {
-    arr = new int[len];
+    int *arr = new int[len];
     srand(time(0));
     for (int i = 0; i < len; i++)
     {
         arr[i] = rand() % 100; // generate a random integer between 0 and 99
     }
+    return arr;
 }
---
+++ search.hpp
@@ -0,0 +1,7 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include "ternarySearch.cpp"
+#include "binarySearch.cpp"
+
+#endif
